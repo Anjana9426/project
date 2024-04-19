ebc251f0ef40f6283cb0a3b1bb85ba636a34b19c
Anjana9426 <165657663+Anjana9426@users.noreply.github.com>
2024-04-19 10:05:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
-import { Route, Router } from 'react-router-dom';
+import View from './Components/View';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     </div>
+      <View/>
+    </div>
   );
 }
 
